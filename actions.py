d2035d3b17cdf11c6638569b59385f9690a14493
lilyBrainy <m.o.kh.alenzii@gmail.com>
2019-01-16 15:02:18 +0300

    Project3

--- actions.py
+++ actions.py
@@ -2,35 +2,124 @@
 from data import  population, clubs
 from components import Club, Person
 
-my_name = ""
-my_age = -1
-my_bio = ""
+my_name = "Marwah"
+my_age = 23
+my_bio = "I'm Awesome"
 myself = Person(my_name, my_bio, my_age)
+
 
 def introduction():
     print("Hello, %s. Welcome to our portal." % my_name)
 
 def options():
     # your code goes here!
+    print("-----------------")
+    print('Would you like to:')
+    print("1) Create a new club \n 2) Browse and join clubs \n 3) View existing clubs \n 4) Display members of a club \n -1) Close application" )
+    option = ''
+    option = input()
+    if(option == "Create a new club" or option == "1"):
+    	create_club()
+    elif(option == "Browse and join clubs" or option == "2"):
+    	view_clubs()
+    elif(option == "View existing clubs" or option == "3"):
+    	view_club_members()
+    elif(option == "Display members of a club" or option == "4"):
+    	join_clubs()
+    elif(option == "Close application" or option == "-1"):
+    	print("app is close")			
+    	
+
     
 
 def create_club():
     # your code goes here!
+    name = input("Pick a name for your awesome new club: " )
+    description = input("What is your club about: ")
+
+    new_club = Club(name, description)
+    new_club.assign_president(myself)
+    
+    print("Enter the number of people you would like to recruit to your new club (-1 to stop")
+    user_input = ''
+    index = 1
+    # example = range(len(population))
+    # print("Here is the example:")
+    # print(example)
+    for person in population:
+        print("[%s] %s" % (index, person))
+        index += 1
+        # print("[%s] %s" % (person, population[person]))
+    while(user_input != "-1"):
+    	user_input = input()
+    	if(user_input !="-1"):
+    		for person in population:
+    			
+    			target_index = str(population.index(person)+1)
+    			
+    			if (user_input == target_index):
+    				new_club.recruit_member(person)
+    				
+    			# if user_input == str(member_number):
+    			# 	new_club.recruit_member(member)
+    				print(str(person) + " is added to the Club " + name)
+
+    		
+
+    print("Here's your club:")
+    print("Club Name: " + new_club.name)
+    print("Club Description: " + new_club.description)
+    print("Members: ")
+    # print(new_club.president)
+    new_club.print_member_list()
+    # print(" %s ( %s years old, President ) -  %s" % (, str(my_age),my_bio))
+    # print(new_club.recruit_member())
+
+    
+    
+
     
 
 def view_clubs():
     # your code goes here!
-    
+    # print(clubName)
+    # print(clubDescription)   
+    for club in clubs:
+    	print()
+    	print(club)
+    user_input = input("Enter the name of the club you would like to join: ")
+    for club in clubs:
+    	if user_input.lower() == club.name.lower() :
+    		print(my_name + " just joined: " + user_input)
+    		club.members.append(myself)
+    		print(club)
+
 
 def view_club_members():
     # your code goes here!
+    for club in clubs:
+    	print()
+    	print(club)
     
 
 def join_clubs():
     # your code goes here!
+    # for club in clubs:
+    # 	print(club)
+    view_club_members()
+    user_input = input("Enter the name of the club whose members you would like to see: ")
+    for club in clubs:
+    	if (user_input.lower() == club.name.lower()):
+            print("Members of the %s Are:" %(user_input) )
+            print(club.print_member_list());  
+    		
+    		
+            
+
+    	
     
 
 def application():
     introduction()
     # your code goes here!
-    
+    options()
--- components.py
+++ components.py
@@ -2,21 +2,41 @@
 class Person():
     def __init__(self, name, bio, age):
         # your code goes here!
+        self.name = name
+        self.bio = bio
+        self.age = age
+
+    def __str__(self):
+        return "%s %s %s" % (self.name, self.age, self.bio)
 
 
 class Club():
     def __init__(self, name, description):
         # your code goes here!
+        self.name = name
+        self.description = description
+        self.members = []
+        self.president = {}
 
 
     def assign_president(self, person):
         # your code goes here!
+        self.president = person
+
 
 
     def recruit_member(self, person):
         # your code goes here!
+        self.members.append(person)
 
 
     def print_member_list(self):
         # your code goes here!
+        for member in self.members:
+            print(member)
+        print(str(self.president) + " (PRESIDENT OF THE CLUB)" )   
+
+
+    def __str__(self):
+        return "Name: %s \nDescription: %s \nMembers: %s" %(self.name , self.description, len(self.members))
 
